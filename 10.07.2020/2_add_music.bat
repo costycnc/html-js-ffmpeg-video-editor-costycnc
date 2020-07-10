5c73957d0d6ab29438e753bca8f8495a2ac619a8
Boboaca Costel <bobyca2003@yahoo.com>
2020-07-10 16:12:05 +0200

    Update 2_add_music.bat

--- 10.07.2020/2_add_music.bat
+++ 10.07.2020/2_add_music.bat
@@ -1,4 +1,4 @@
-IF EXIST output.mp4 DEL /F output.mp4
+IF EXIST output1.mp4 DEL /F output1.mp4
 ffmpeg -i _output_test.mp4 -i mp3_mp4\music.mp3 -c copy -shortest -map 0:v:0 -map 1:a:0 output1.mp4
 DEL _output_test.mp4
 Pause 0
